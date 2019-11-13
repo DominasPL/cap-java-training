f44aa4b992add5f07cb01d8e556535dad732e588
DominasPL <dominik-stepuch@wp.pl>
2019-11-13 20:31:51 +0100

    Exercise 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import pl.github.DominasPL.methods.MyString;
 import pl.github.DominasPL.methods.Temperature;
 import pl.github.DominasPL.objects.Dog;
 import pl.github.DominasPL.objects.Owner;
@@ -5,15 +6,19 @@ import pl.github.DominasPL.objects.Owner;
 public class Main {
 
     public static void main(String[] args) {
+//
+//        //Exercise1
+//        System.out.println("Temperature is greater than 0: " + Temperature.checkIsPositive(-20.0));
+//
+//        //Exercise2
+//        Owner owner = new Owner("Arkadiusz", "Gejsheimer");
+//        Dog dog = new Dog("Fafik", 20, owner);
+//        System.out.println(dog);
 
-        //Exercise1
-        System.out.println("Temperature is greater than 0: " + Temperature.checkIsPositive(-20.0));
-
-        //Exercise2
-        Owner owner = new Owner("Arkadiusz", "Gejsheimer");
-        Dog dog = new Dog("Fafik", 20, owner);
-        System.out.println(dog);
+        //Exercise3
+        MyString.displayTextAndCountLen("Today is rainny day", "There is too much snow outside", "We have sunny, hot day");
 
     }
+
 
 }
---
+++ src/main/java/pl/github/DominasPL/methods/MyString.java
@@ -0,0 +1,12 @@
+package pl.github.DominasPL.methods;
+
+public class MyString {
+
+    public static void displayTextAndCountLen(String w1, String w2, String w3) {
+        System.out.println(w1);
+        System.out.println(w2);
+        System.out.println(w3);
+        System.out.println("Length of all strings = " + (w1.length() + w2.length() + w3.length()));
+    }
+
+}
