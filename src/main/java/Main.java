6741b9f1f03be67b142be07d6c384870b2f1f5aa
DominasPL <dominik-stepuch@wp.pl>
2019-11-13 19:13:05 +0100

    Exercise 1, java course, methods

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import methods.Temperature;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Exercise1
+        System.out.println("Temperature is greater than 0: " + Temperature.checkIsPositive(-20.0));
+
+
+    }
+
+
+}
---
+++ src/main/java/methods/Temperature.java
@@ -0,0 +1,9 @@
+package methods;
+
+public class Temperature {
+
+    public static boolean checkIsPositive(double temperature) {
+        return temperature > 0;
+    }
+
+}
